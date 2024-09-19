16bd10119ddfc145bd86936cecf7677fcfce4b26
AhmarRa <ahmar.ali.rathore@gmail.com>
2024-09-19 15:09:40 +0100

    first commit

---
+++ week-2/day-4/notes/libraries.py
@@ -0,0 +1,14 @@
+import random
+# imports entire random library
+
+# random float between 0 and 1 eg 0.567233900626328
+print(random.random())
+
+# random float between 1 and 10 eg 1.4615378077421968
+print(random.uniform(1,10))
+
+# random integer between 1 and 10 
+print(random.randint(1,10))
+
+# random integer between 1 and 6 -> like a die for a game.
+print(random.randint(1,6))
